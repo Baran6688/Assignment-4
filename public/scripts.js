c4495ff75ce54fdf1a9af0ec15f0990122ec2a78
Baran <baranali6688@gmail.com>
2023-12-25 18:41:39 +0300

    Changing to the serverURL

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("#form")
 const blogsDiv = document.querySelector(".posts")
 const errDiv = document.querySelector("#error")
-const socket = io("ws://localhost:3000")
+const socket = io("https://pureblogz.onrender.com")
 
 // Blogs
 let blogs = []
